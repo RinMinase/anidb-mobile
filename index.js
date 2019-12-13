d8db8183bfba403876558eb18f896f39d88d02b1
Rin Minase <rin.black.raison.detre@gmail.com>
2019-12-13 22:15:38 +0800

    ANIDB: removed app json file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App/App';
-import {name as appName} from './app.json';
+
+const appName = AniDBMobile
 
 AppRegistry.registerComponent(appName, () => App);
