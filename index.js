944194173c45c5157756412556573652c1dc64fc
Rin Minase <rin.black.raison.detre@gmail.com>
2019-12-14 00:23:11 +0800

    ANIDB: fixed application name

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import {AppRegistry} from 'react-native';
-import App from './App/App';
+import { AppRegistry } from "react-native";
+import App from "./App/App";
 
-const appName = AniDBMobile
+const appName = "anidb";
 
 AppRegistry.registerComponent(appName, () => App);
